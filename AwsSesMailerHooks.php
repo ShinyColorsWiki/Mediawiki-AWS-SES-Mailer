dafde4fdfe69ea7e319f1c4c9954fddc108ebdf9
MPThLee <mpthlee@gmail.com>
2022-12-24 14:28:57 +0900

    feat: :art: PHPCS and make php 8.1 support

--- AwsSesMailerHooks.php
+++ AwsSesMailerHooks.php
@@ -1,72 +1,70 @@
 <?php
 
-use \Aws\Ses\SesClient;
-use \Aws\Exception\AwsException;
+use Aws\Exception\AwsException;
+use Aws\Ses\SesClient;
 
 class AwsSesMailerHooks {
-    /**
-     * Send a mail using AWS SES API
-     *
-     * @param array $headers
-     * @param array $to
-     * @param MailAddress $from
-     * @param string $subject
-     * @param string $body
-     * @return bool
-     * @throws Exception
-     */
-    public static function onAlternateUserMailer(
-        array $headers,
-        array $to,
-        MailAddress $from,
-        $subject,
-        $body
-    ) {
-        global $wgAwsSesCredentials, $wgAwsSesRegion;
+	/**
+	 * Send a mail using AWS SES API
+	 *
+	 * @param array $headers
+	 * @param array $to
+	 * @param MailAddress $from
+	 * @param string $subject
+	 * @param string $body
+	 * @return bool
+	 * @throws Exception
+	 */
+	public static function onAlternateUserMailer(
+		array $headers,
+		array $to,
+		MailAddress $from,
+		$subject,
+		$body
+	) {
+		global $wgAwsSesCredentials, $wgAwsSesRegion;
 
-        if ( !( $wgAwsSesCredentials && $wgAwsSesRegion ) ) {
-            return true;
-        }
+		if ( !( $wgAwsSesCredentials && $wgAwsSesRegion ) ) {
+			return true;
+		}
 
-        $ses = new SesClient([
-            'version' => 'latest',
-            'region'  => $wgAwsSesRegion,
-            'credentials' => $wgAwsSesCredentials
-        ]);
+		$ses = new SesClient( [
+			'version' => 'latest',
+			'region'  => $wgAwsSesRegion,
+			'credentials' => $wgAwsSesCredentials
+		] );
 
-        $param = array();
+		$param = [];
 
-        foreach( $to as $recipient ) {
-            $param['Destination'] = array(
-                'ToAddresses' => (array) $recipient,
-            );
-        }
+		foreach ( $to as $recipient ) {
+			$param['Destination'] = [
+				'ToAddresses' => (array)$recipient,
+			];
+		}
 
-        $param['Source'] = $from;
-        $param['ReplyToAddresses'] = array (
-            $headers['Return-Path']
-        );
-        $param['Message'] = array(
-            'Subject' => array(
-                'Data' => $subject,
-                'Charset' => 'utf-8',
-            ),
-            'Body' => array(
-                'Text' => array(
-                    'Data' => $body,
-                    'Charset' => 'UTF-8',
-                ),
-            ),
-        );
+		$param['Source'] = $from;
+		$param['ReplyToAddresses'] = [
+			$headers['Return-Path']
+		];
+		$param['Message'] = [
+			'Subject' => [
+				'Data' => $subject,
+				'Charset' => 'utf-8',
+			],
+			'Body' => [
+				'Text' => [
+					'Data' => $body,
+					'Charset' => 'UTF-8',
+				],
+			],
+		];
 
+		try{
+			$ses->sendEmail( $param );
+		} catch ( AwsException $e ) {
+			return $e->getMessage();
+		}
 
-
-        try{
-            $ses->sendEmail($param);
-        } catch( AwsException $e) {
-            return $e->getMessage();
-        }
-
-        return false;
-    }
+		return false;
+	}
 }
